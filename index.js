2ea049898d3f89f16fb00529b2effcd88950a354
Adham14896 <114481321+Adham14896@users.noreply.github.com>
2023-01-02 23:31:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var drums =document.querySelectorAll('.drum').length;
+
+for(var i= 0; i < drums; i++) {
+
+document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+
+const buttonInner = this.innerHTML;
+playSound(buttonInner);
+
+});
+
+document.addEventListener('keypress', function(event){
+    playSound(event.key)
+
+});
+
+
+
+
+
+function playSound(key)  {
+
+        switch (key) {
+    
+            case "w":
+            const crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+            
+            case "a":   
+            const kickBass = new Audio('sounds/kick-bass.mp3');
+            kickBass.play();
+            break;
+            
+            case "s":
+            const snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+            
+            case "d":
+            const tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+            
+            case "j":
+            const tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+            
+            case "k": 
+            const tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+            
+            case "l":
+            const tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+            
+            default:
+            
+            }
+    }
+}
